8449c066e31d550e0ee70aa7f2741197a53d493f
gmcmurray <83099840+gmcmurray@users.noreply.github.com>
2021-09-23 12:07:11 -0700

    added / route to get index.html in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 const app = express();
 
@@ -21,7 +21,12 @@ mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost/budget", {
 });
 
 // routes
+
 app.use(require("./routes/api.js"));
+
+app.get("/",  (req, res) => {
+  res.sendFile(path.join(__dirname, '/public', 'index.html'));
+})
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
